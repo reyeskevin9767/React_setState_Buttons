96c3eaa2244b7b4eb573a961b6e86c4cae0e1f2b
reyeskevin9767 <reyeskevin9767@gmail.com>
2019-05-24 17:21:53 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from './Button';
+import './App.css';
+import BrokenClick from './BrokenClick';
+import BrokenClick2 from './BrokenClick2';
+
+function App() {
+  return (
+    <div className="App">
+        <BrokenClick />
+        <BrokenClick2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BrokenClick2.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class BrokenClick2 extends Component{
+
+    state = {clicked: false};
+
+    handleClick = (e) => {
+        this.setState({clicked: true});
+    }
+
+    render(){
+        return (
+            <div>
+            <h1>
+                {this.state.clicked ? 'Clicked!' : 'Not Clicked'}
+            </h1>
+            <button onClick={this.handleClick}>Cilcked ME</button>                
+            </div>
+
+        )
+    }
+}
+
+export default BrokenClick2;
